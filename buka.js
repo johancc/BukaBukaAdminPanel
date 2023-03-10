301f097e6395b77ce9efd816655ed07e24952cf3
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2023-03-10 11:37:04 -0500

    add endpoint

--- buka.js
+++ buka.js
@@ -1,7 +1,9 @@
+const ENDPOINT = "https://buka-buka.onrender.com"
+
 // Step 1: 
 
 // Create a function that makes a POST request to buka's wake up API endpoint
-
+// Use the Fetch API, documentation: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 
 // Step 2:
 //      Make it so when the "wake-up button" is clicked, then function from step 1 is called
@@ -12,6 +14,3 @@
 
 // Step 4 (optional): Make a POST request which sets buka's happiness
 //  (hint: you'll need to get the value of the input field)
-
-
-
