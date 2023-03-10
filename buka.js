0b2ef7c005c8227557dd42da0734dde5696d7aa3
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2023-03-10 11:41:07 -0500

    redefine steps

--- buka.js
+++ buka.js
@@ -1,16 +1,16 @@
 const ENDPOINT = "https://buka-buka.onrender.com"
 
 // Step 1: 
-
 // Create a function that makes a POST request to buka's wake up API endpoint
-// Use the Fetch API, documentation: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 
-// Step 2:
-//      Make it so when the "wake-up button" is clicked, then function from step 1 is called
+  
 
-
-// Step 3: Repeat steps 1-2 to make the sleep button work
+// Step 3: Make a sleep() function which calls buka's sleep endpoint
 
 
 // Step 4 (optional): Make a POST request which sets buka's happiness
-//  (hint: you'll need to get the value of the input field)
+//  (hint: you'll need to get the value of the input field with id "happiness")
+
+async function sendHappines() {
+    // Your logic here :) 
+}
