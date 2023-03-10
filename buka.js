edd236af4ff587df9c61068c686d7bf4fe07bb68
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2023-03-10 12:09:27 -0500

    added hints

--- buka.js
+++ buka.js
@@ -1,16 +1,39 @@
+/**
+ * Important
+ *  You might encounter an error with the word "CORS" in it.
+ *  This means that your browser is preventing you from sending post request to another endpoint.
+ *  This is an important security measure, but for this exercise, we want to bypass it.
+ * 
+ * You'll have to modify your fetch request like so:
+ *      fetch("/some/endpoint", {
+ *          method: "POST"
+ *          mode: "no-cors",
+ *          // the rest of your fetch parameters here.
+ *      })
+ * 
+ */
 const ENDPOINT = "https://buka-buka.onrender.com"
 
 // Step 1: 
 // Create a function that makes a POST request to buka's wake up API endpoint
 
-  
+// Step 2 is in the html file :)
 
 // Step 3: Make a sleep() function which calls buka's sleep endpoint
 
-
 // Step 4 (optional): Make a POST request which sets buka's happiness
 //  (hint: you'll need to get the value of the input field with id "happiness")
+async function sendHappiness() {
+  const params = {}; // Set your params
+  // Don't worry about this :)
+  const body = Object.keys(params).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(params[key])).join('&');
 
-async function sendHappines() {
-    // Your logic here :) 
+  return await fetch("CHANGE_ME", {
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' // specially not this :)
+      },
+    mode: "no-cors", // nor this :)
+    body: body
+  });
 }
