87e4078f889bb7150c49b37eec5c444810c443c0
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2023-03-10 11:30:39 -0500

    admin panel

---
+++ buka.js
@@ -0,0 +1,17 @@
+// Step 1: 
+
+// Create a function that makes a POST request to buka's wake up API endpoint
+
+
+// Step 2:
+//      Make it so when the "wake-up button" is clicked, then function from step 1 is called
+
+
+// Step 3: Repeat steps 1-2 to make the sleep button work
+
+
+// Step 4 (optional): Make a POST request which sets buka's happiness
+//  (hint: you'll need to get the value of the input field)
+
+
+
